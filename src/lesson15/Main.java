f93abaf2f7823e319fba1781bbbd86e4fcb691b4
Marina <badbilliejoe@gmail.com>
2021-06-21 09:37:40 +0300

    lesson about exceptions2

---
+++ src/lesson15/Main.java
@@ -0,0 +1,13 @@
+package lesson15;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        try {
+            person.setAge(150);
+        } catch (UncurrentAgeException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Программа работает");
+    }
+}
---
+++ src/lesson15/StringUtilsImpl.java
@@ -0,0 +1,20 @@
+package lesson15;
+
+public class StringUtilsImpl implements StringUtils {
+    @Override
+    public double div(String number1, String number2) throws NullPointerException, NumberFormatException, ArithmeticException {
+
+
+        return 0;
+    }
+
+    @Override
+    public int[] findWord(String text, String word) throws NullPointerException {
+        return new int[0];
+    }
+
+    @Override
+    public double[] findNumbers(String text) throws CustomException {
+        return new double[0];
+    }
+}
