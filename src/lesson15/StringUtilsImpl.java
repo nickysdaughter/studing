5936655f8a530dbca4ad08a00cadea3d8069b5fe
Marina <badbilliejoe@gmail.com>
2021-06-21 18:52:09 +0300

    homework exceptions

--- src/lesson15/StringUtilsImpl.java
+++ src/lesson15/StringUtilsImpl.java
@@ -3,9 +3,23 @@ package lesson15;
 public class StringUtilsImpl implements StringUtils {
     @Override
     public double div(String number1, String number2) throws NullPointerException, NumberFormatException, ArithmeticException {
+        int intNumber1 = 0;
+        int intNumber2 = 0;
+        try {
+            intNumber1 = Integer.parseInt(number1.trim());
+            intNumber2 = Integer.parseInt(number2.trim());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
 
+        if (number1 == null || number2 == null) {
+            throw new NullPointerException("number1 или number2 равны null");
+        } else if (intNumber2 == 0) {
+            throw new ArithmeticException("number2 равно нулю");
+        }
 
-        return 0;
+        double div = (double) intNumber1 / intNumber2;
+        return div;
     }
 
     @Override
