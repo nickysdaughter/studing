833e3ce2947a8eca6e617e678f34a35bd4ae44fa
Marina <badbilliejoe@gmail.com>
2021-06-28 21:37:06 +0300

    added 2 methods for exceptions

--- src/lesson15/Main2.java
+++ src/lesson15/Main2.java
@@ -1,10 +1,12 @@
 package lesson15;
 
+import java.util.Arrays;
+
 public class Main2 {
     public static void main(String[] args) {
 
         StringUtilsImpl utils = new StringUtilsImpl();
-        double div = 0.0;
+        /*double div = 0.0;
         try {
             div = utils.div("1", "2");
         }
@@ -14,5 +16,19 @@ public class Main2 {
 
         System.out.println(div);
 
+         */
+        try {
+            utils.findWord("мама мыла раму", "мама");
+        }
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+       try {
+            utils.findNumbers("Число π - это 3.14");
+        }
+        catch (CustomException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/lesson15/StringUtilsImpl.java
+++ src/lesson15/StringUtilsImpl.java
@@ -1,20 +1,20 @@
 package lesson15;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringUtilsImpl implements StringUtils {
     @Override
     public double div(String number1, String number2) throws NullPointerException, NumberFormatException, ArithmeticException {
         int intNumber1 = 0;
         int intNumber2 = 0;
-        try {
-            intNumber1 = Integer.parseInt(number1.trim());
-            intNumber2 = Integer.parseInt(number2.trim());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-
         if (number1 == null || number2 == null) {
             throw new NullPointerException("number1 или number2 равны null");
-        } else if (intNumber2 == 0) {
+        }
+        intNumber1 = Integer.parseInt(number1.trim());
+        intNumber2 = Integer.parseInt(number2.trim());
+
+        if (intNumber2 == 0) {
             throw new ArithmeticException("number2 равно нулю");
         }
 
@@ -24,11 +24,34 @@ public class StringUtilsImpl implements StringUtils {
 
     @Override
     public int[] findWord(String text, String word) throws NullPointerException {
+        if (text == null || word == null) {
+            throw new NullPointerException("искомое слово отсутствует в тексте или текст пустой");
+        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        String[] newText = text.split(" ");
+        for (int i = 0; i < newText.length; i++) {
+            String result = newText[i];
+            if (result.equals(word)) {
+                arrayList.add(result.indexOf(i));
+                Integer[] newArray = arrayList.toArray(new Integer[arrayList.size()]);
+                System.out.println(Arrays.toString(newArray));
+            }
+        }
         return new int[0];
     }
 
     @Override
     public double[] findNumbers(String text) throws CustomException {
+        /*if () {
+            throw new CustomException("???");  //ЧТО-ТО НЕ ТАК! ДОДЕЛАТЬ!>:|
+        }*/
+        double[] numbers = new double[0];
+        String[] numbersStrings = text.split(".");
+        for (int i = 0; i < numbersStrings.length; i++) {
+            numbers[i] = Double.parseDouble(numbersStrings[i]);
+        }
+        System.out.println(Arrays.toString(numbers));
+
         return new double[0];
     }
 }
